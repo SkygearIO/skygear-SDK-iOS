8a1eac883aecb392f41f4bb54e5cdc9921bca490
Rick Mak <rick.mak@gmail.com>
2015-03-03 19:05:53 +0800

    Fix call the wrong db on container init 61c0e2d

--- Pod/Classes/ODContainer.m
+++ Pod/Classes/ODContainer.m
@@ -36,7 +36,7 @@ NSString *const ODContainerRequestBaseURL = @"http://localhost:5000/v1";
         _operationQueue = [[NSOperationQueue alloc] init];
         _operationQueue.name = @"ODContainerOperationQueue";
         _publicCloudDatabase = [[ODDatabase alloc] initPrivately];
-        _privateCloudDatabase.databaseID = @"_public";
+        _publicCloudDatabase.databaseID = @"_public";
         _privateCloudDatabase = [[ODDatabase alloc] initPrivately];
         _privateCloudDatabase.databaseID = @"_private";
         
