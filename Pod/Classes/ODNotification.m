fc62a8dfb4a403e28f597436eaeceaee9557835b
Kenji Pa <limouren@gmail.com>
2015-05-19 15:52:07 +0800

    Build ODNotification from the userinfo dictionary provided
    Currently only deserialization of subscription id is implemented.
    
    Ref #70.

--- Pod/Classes/ODNotification.m
+++ Pod/Classes/ODNotification.m
@@ -6,8 +6,33 @@
 //  Copyright (c) 2015 Rocky Chan. All rights reserved.
 //
 
-#import "ODNotification.h"
+#import "ODNotification_Private.h"
+
+@interface ODNotification()
+
+@property (nonatomic, readwrite, copy) ODNotificationID *notificationID;
+@property (nonatomic, readwrite, assign) ODNotificationType notificationType;
+
+@end
 
 @implementation ODNotification
+
++ (instancetype)notificationFromRemoteNotificationDictionary:(NSDictionary *)notificationDictionary
+{
+    NSDictionary *info = notificationDictionary[@"_ourd"];
+    if (!info) {
+        return nil;
+    }
+
+    ODNotification *notification = [[self alloc] init];
+    // notificationID not implemented, every notification is different.
+    // TODO(limouren): implement notificationID when fetch notification is needed.
+    notification.notificationID = [[ODNotificationID alloc] init];
+    // we only have query notification at the moment
+    notification.notificationType = ODNotificationTypeQuery;
+
+    notification.subscriptionID = info[@"subscription-id"];
+    return notification;
+}
 
 @end
---
+++ Pod/Classes/ODNotification_Private.h
@@ -0,0 +1,15 @@
+//
+//  ODNotification_Private
+//  ;
+//
+//  Created by Kenji Pa on 29/1/15.
+//  Copyright (c) 2015 Rocky Chan. All rights reserved.
+//
+
+#import "ODNotification.h"
+
+@interface ODNotification()
+
+@property (nonatomic, readwrite, copy) NSString *subscriptionID;
+
+@end
