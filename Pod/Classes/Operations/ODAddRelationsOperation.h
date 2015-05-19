6ea7139ee7d51e7a08a155d946e73384cee17f04
Rick Mak <rick.mak@gmail.com>
2015-05-19 17:05:45 +0800

    Add AddRelationsOperation #87

---
+++ Pod/Classes/Operations/ODAddRelationsOperation.h
@@ -0,0 +1,45 @@
+//
+//  ODAddRelationOperation.h
+//  Pods
+//
+//  Created by Rick Mak on 18/5/15.
+//
+//
+
+#import "ODOperation.h"
+
+@interface ODAddRelationsOperation : ODOperation
+
+
+/**
+ Instantiates an instance of <ODAddRelationsOperation> with a list of user to be related with current user.
+ 
+ @param records An array of users to be related.
+ */
+- (instancetype)initWithType:(NSString *)relationType andUsersToRelated:(NSArray *)users;
+
+/**
+ Type of the relation, default provide `follow` and `friend`.
+ */
+@property (nonatomic, copy) NSString *relationType;
+
+/**
+ Sets or returns an array of users to be related.
+ */
+@property (nonatomic, copy) NSArray *usersToRelate;
+
+
+/**
+ Sets or returns a block to be called when the save operation for individual record is completed.
+ If an error occurred during the save, the <NSError> will be specified.
+ */
+@property (nonatomic, copy) void (^perUserCompletionBlock)(ODUserRecordID *userID, NSError *error);
+
+/**
+ Sets or returns a block to be called when the entire operation completes. If the entire operation
+ results in an error, the <NSError> will be specified.
+ */
+@property (nonatomic, copy) void (^addRelationsCompletionBlock)(NSArray *savedUserIDs, NSError *operationError);
+
+
+@end
