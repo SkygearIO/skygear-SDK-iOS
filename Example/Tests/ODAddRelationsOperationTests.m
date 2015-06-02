586778b7db0e6a91814df9b99da2dd37ed57f99e
Kenji Pa <limouren@gmail.com>
2015-06-02 15:47:47 +0800

    Amend expected response of ODAddRelationsOperation

--- Example/Tests/ODAddRelationsOperationTests.m
+++ Example/Tests/ODAddRelationsOperationTests.m
@@ -15,16 +15,19 @@ SpecBegin(ODAddRelationsOperation)
 describe(@"relation add", ^{
     __block ODUser *follower1 = nil;
     __block ODUser *follower2 = nil;
+    __block ODUser *follower3 = nil;
     __block ODContainer *container = nil;
     
     beforeEach(^{
         container = [[ODContainer alloc] init];
         [container updateWithUserRecordID:[ODUserRecordID recordIDWithUsername:@"USER_ID"]
                               accessToken:[[ODAccessToken alloc] initWithTokenString:@"ACCESS_TOKEN"]];
-        ODUserRecordID *userRecordID = [ODUserRecordID recordIDWithUsername:@"user1001"];
-        follower1 = [[ODUser alloc] initWithUserRecordID:userRecordID];
-        userRecordID = [ODUserRecordID recordIDWithUsername:@"user1002"];
-        follower2 = [[ODUser alloc] initWithUserRecordID:userRecordID];
+        ODUserRecordID *userRecordID1 = [ODUserRecordID recordIDWithUsername:@"user1001"];
+        follower1 = [[ODUser alloc] initWithUserRecordID:userRecordID1];
+        ODUserRecordID *userRecordID2 = [ODUserRecordID recordIDWithUsername:@"user1002"];
+        follower2 = [[ODUser alloc] initWithUserRecordID:userRecordID2];
+        ODUserRecordID *userRecordID3 = [ODUserRecordID recordIDWithUsername:@"user1003"];
+        follower3 = [[ODUser alloc] initWithUserRecordID:userRecordID3];
     });
     
     it(@"multiple relations", ^{
@@ -39,13 +42,13 @@ describe(@"relation add", ^{
         expect(request.payload[@"targets"]).to.haveCountOf(2);
         expect(request.accessToken).to.equal(container.currentAccessToken);
         
-        expect(request.payload[@"targets"][0]).to.equal(@"user/user1001");
-        expect(request.payload[@"targets"][1]).to.equal(@"user/user1002");
+        expect(request.payload[@"targets"][0]).to.equal(@"user1001");
+        expect(request.payload[@"targets"][1]).to.equal(@"user1002");
     });
     
     it(@"make request", ^{
         ODAddRelationsOperation *operation = [[ODAddRelationsOperation alloc] initWithType:@"follow"
-                                                                         usersToRelated:@[follower1, follower2]];
+                                                                         usersToRelated:@[follower1, follower2, follower3]];
         
         [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
             return YES;
@@ -54,15 +57,24 @@ describe(@"relation add", ^{
                                          @"request_id": @"REQUEST_ID",
                                          @"result": @[
                                                  @{
-                                                     @"_id": @"user/user1001",
+                                                     @"id": @"user1001",
+                                                     @"type": @"user",
+                                                     @"data": @{
+                                                             @"_id": @"user1001",
+                                                             @"email": @"user1001@example.com",
+                                                             },
                                                      },
                                                  @{
-                                                     @"_id": @"user/user1002",
-                                                     @"_type": @"error",
-                                                     @"message": @"cannot find user",
-                                                     @"code": @"ResourceNotFound",
-                                                     }
-                                                 ]
+                                                     @"id": @"user1002",
+                                                     @"type": @"error",
+                                                     @"data": @{
+                                                             @"code": @104,
+                                                             @"message": @"cannot find user",
+                                                             @"type": @"ResourceNotFound",
+                                                             @"info": @{@"id": @"user1002"},
+                                                             },
+                                                     },
+                                                 ],
                                          };
             NSData *payload = [NSJSONSerialization dataWithJSONObject:parameters
                                                               options:0
@@ -74,12 +86,15 @@ describe(@"relation add", ^{
         }];
         
         waitUntil(^(DoneCallback done) {
-            operation.addRelationsCompletionBlock = ^(NSArray *savedRecords, NSError *operationError) {
+            operation.addRelationsCompletionBlock = ^(NSArray *savedUsers, NSError *operationError) {
                 dispatch_async(dispatch_get_main_queue(), ^{
-                    expect([savedRecords class]).to.beSubclassOf([NSArray class]);
-                    expect(savedRecords).to.haveCountOf(1);
-                    expect(savedRecords[0]).to.equal(follower1.recordID);
+                    expect([savedUsers class]).to.beSubclassOf([NSArray class]);
+                    expect(savedUsers).to.haveCountOf(1);
+                    expect([savedUsers[0] recordID]).to.equal([ODUserRecordID recordIDWithUsername:@"user1001" email:@"user1001@example.com"]);
                     expect(operationError).toNot.beNil();
+                    NSArray *errorKeys = [operationError.userInfo[ODPartialErrorsByItemIDKey] allKeys];
+                    expect(errorKeys).to.contain(@"user1002");
+                    expect(errorKeys).to.contain(@"user1003");
                     done();
                 });
             };
--- Pod/Classes/Operations/ODAddRelationsOperation.m
+++ Pod/Classes/Operations/ODAddRelationsOperation.m
@@ -10,6 +10,7 @@
 
 #import "ODDataSerialization.h"
 #import "ODError.h"
+#import "ODUserRecordID_Private.h"
 
 @implementation ODAddRelationsOperation
 
@@ -22,6 +23,15 @@
     return self;
 }
 
+- (NSArray /* NSString */ *)userStringIDs
+{
+    NSMutableArray *ids = [NSMutableArray arrayWithCapacity:self.usersToRelate.count];
+    for (ODUser *user in self.usersToRelate) {
+        [ids addObject:user.username];
+    }
+    return ids;
+}
+
 - (void)prepareForRequest
 {
     NSMutableDictionary *payload = [@{
@@ -29,47 +39,11 @@
                                      } mutableCopy];
     NSMutableArray *targets = [NSMutableArray array];
     for (ODUser *user in self.usersToRelate) {
-        [targets addObject:user.recordID.canonicalString];
+        [targets addObject:user.username];
     }
     payload[@"targets"] = targets;
     self.request = [[ODRequest alloc] initWithAction:@"relation:add" payload:payload];
     self.request.accessToken = self.container.currentAccessToken;
-}
-
-- (NSArray *)processResultArray:(NSArray *)result operationError:(NSError **)operationError
-{
-    NSMutableArray *savedUserIDs = [NSMutableArray array];
-    NSMutableDictionary *errorByUserID = [NSMutableDictionary dictionary];
-    [result enumerateObjectsUsingBlock:^(NSDictionary *obj, NSUInteger idx, BOOL *stop) {
-        NSString *objType = obj[@"_type"];
-        NSString *userID = obj[@"_id"];
-        ODUserRecordID *userRecordID = [[ODUserRecordID alloc] initWithCanonicalString:userID];
-        if ([objType isEqual:@"error"]) {
-            NSMutableDictionary *userInfo = [ODDataSerialization userInfoWithErrorDictionary:obj];
-            userInfo[NSLocalizedDescriptionKey] = @"An error occurred while adding relation.";
-            NSError *error = [NSError errorWithDomain:ODOperationErrorDomain
-                                                code:0
-                                            userInfo:userInfo];
-            errorByUserID[userID] = error;
-            if (self.perUserCompletionBlock) {
-                self.perUserCompletionBlock(userRecordID, error);
-            }
-        } else {
-            [savedUserIDs addObject:userRecordID];
-            if (self.perUserCompletionBlock) {
-                self.perUserCompletionBlock(userRecordID, nil);
-            }
-            
-        }
-    }];
-    if (errorByUserID.count) {
-        *operationError = [NSError errorWithDomain:ODOperationErrorDomain
-                                             code:ODErrorPartialFailure
-                                         userInfo:@{
-                                                    ODPartialErrorsByItemIDKey: errorByUserID,
-                                                    }];
-    }
-    return savedUserIDs;
 }
 
 - (void)handleRequestError:(NSError *)error
@@ -82,21 +56,94 @@
 - (void)handleResponse:(NSDictionary *)response
 {
     NSArray *result = response[@"result"];
-    NSArray *savedUserIDs = nil;
-    NSError *error = nil;
-    if ([result isKindOfClass:[NSArray class]]) {
-        savedUserIDs = [self processResultArray:result operationError:&error];
-    } else {
-        savedUserIDs = [NSArray array];
+    if (![result isKindOfClass:[NSArray class]]) {
         NSDictionary *userInfo = [self errorUserInfoWithLocalizedDescription:@"Server returned malformed result."
                                                              errorDictionary:nil];
-        error = [NSError errorWithDomain:ODOperationErrorDomain
+        NSError *error = [NSError errorWithDomain:ODOperationErrorDomain
                                     code:0
                                 userInfo:userInfo];
+        if (self.addRelationsCompletionBlock) {
+            self.addRelationsCompletionBlock(nil, error);
+        }
+        return;
     }
+
+    NSDictionary *itemsByID = [self.class itemsByIDFromResult:result];
+
+    NSMutableArray *savedUsers = [NSMutableArray arrayWithCapacity:itemsByID.count];
+    NSMutableDictionary *errorsByStringUserID = [NSMutableDictionary dictionary];
+    for (ODUser *user in self.usersToRelate) {
+        NSDictionary *itemDict = itemsByID[user.username];
+
+        ODUserRecordID *returnedUserID = nil;
+        NSError *error = nil;
+
+        if (!itemDict.count) {
+            NSDictionary *info = @{
+                                   ODErrorCodeKey: @104,
+                                   ODErrorTypeKey: @"ResourceNotFound",
+                                   ODErrorMessageKey: @"User missing in response",
+                                   ODErrorInfoKey: @{@"id": user.username},
+                                   };
+            error = [NSError errorWithDomain:ODOperationErrorDomain
+                                        code:0
+                                    userInfo:info];
+        } else {
+            NSString *itemType = itemDict[@"type"];
+            if ([itemType isEqualToString:@"error"]) {
+                NSDictionary *info = [ODDataSerialization userInfoWithErrorDictionary:itemDict[@"data"]];
+                error = [NSError errorWithDomain:ODOperationErrorDomain
+                                            code:0
+                                        userInfo:info];
+            } else {
+                returnedUserID = [ODUserRecordID recordIDWithUsername:user.username];
+                if (returnedUserID == nil) {
+                    NSDictionary *info = [self errorUserInfoWithLocalizedDescription:@"User does not conform with expected format." errorDictionary:nil];
+                    error = [NSError errorWithDomain:ODOperationErrorDomain
+                                                code:0
+                                            userInfo:info];
+                }
+            }
+        }
+
+        NSAssert(
+                 (returnedUserID == nil && error != nil) ||
+                 (returnedUserID != nil && error == nil),
+                 @"either one from user and error is not nil");
+
+        if (self.perUserCompletionBlock) {
+            self.perUserCompletionBlock(returnedUserID, error);
+        }
+
+        if (returnedUserID != nil) {
+            [savedUsers addObject:returnedUserID];
+        } else {
+            errorsByStringUserID[user.username] = error;
+        }
+    }
+
     if (self.addRelationsCompletionBlock) {
-        self.addRelationsCompletionBlock(savedUserIDs, error);
+        NSError *operationError = nil;
+        if (errorsByStringUserID.count) {
+            operationError = [NSError errorWithDomain:ODOperationErrorDomain
+                                                 code:ODErrorPartialFailure
+                                             userInfo:@{
+                                                        ODPartialErrorsByItemIDKey: errorsByStringUserID,
+                                                        }];
+        }
+
+        self.addRelationsCompletionBlock(savedUsers, operationError);
     }
+}
+
++ (NSDictionary *)itemsByIDFromResult:(NSArray *)result
+{
+    NSMutableDictionary *itemsByID = [NSMutableDictionary dictionaryWithCapacity:result.count];
+    for (NSDictionary *itemDict in result) {
+        NSString *itemID = itemDict[@"id"];
+        itemsByID[itemID] = itemDict;
+    }
+    return itemsByID;
 }
 
 @end
