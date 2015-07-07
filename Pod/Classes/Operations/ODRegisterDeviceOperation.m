328dd7d965ba38dc5609e5db5ce18af0bc6a05fe
Kenji Pa <limouren@Kenjis-MacBook-Pro.local>
2015-07-07 17:30:56 +0800

    Fix ODRegisterDeviceOperation error when no user is logged in.
    Add APIKey to reqeust.

--- Pod/Classes/Operations/ODRegisterDeviceOperation.m
+++ Pod/Classes/Operations/ODRegisterDeviceOperation.m
@@ -61,6 +61,7 @@
 
     self.request = [[ODRequest alloc] initWithAction:@"device:register"
                                              payload:payload];
+    self.request.APIKey = self.container.APIKey;
     self.request.accessToken = self.container.currentAccessToken;
 }
 
