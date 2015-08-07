b81c95cbfa1b5a2e8f6efdddfdd1c752ad21040e
Kwok-kuen Cheung <cheungpat@oursky.com>
2015-08-07 15:21:03 +0800

    Fix OD(FetchRecords/Lambda)OperationTests swapped.

--- Example/Tests/ODFetchRecordsOperationTests.m
+++ Example/Tests/ODFetchRecordsOperationTests.m
@@ -14,98 +14,184 @@ SpecBegin(ODFetchRecordsOperation)
 
 describe(@"fetch", ^{
     __block ODContainer *container = nil;
-
+    __block ODDatabase *database = nil;
+    
     beforeEach(^{
         container = [[ODContainer alloc] init];
         [container updateWithUserRecordID:[ODUserRecordID recordIDWithUsername:@"USER_ID"]
                               accessToken:[[ODAccessToken alloc] initWithTokenString:@"ACCESS_TOKEN"]];
+        database = [container publicCloudDatabase];
     });
-
-    it(@"calls lambda with array args", ^{
-        NSArray *args = @[@"bob"];
-        ODLambdaOperation *operation = [ODLambdaOperation operationWithAction:@"hello:world"
-                                                               arrayArguments:args];
+    
+    it(@"single record", ^{
+        ODRecordID *recordID = [[ODRecordID alloc] initWithRecordType:@"book" name:@"book1"];
+        ODFetchRecordsOperation *operation = [ODFetchRecordsOperation operationWithRecordIDs:@[recordID]];
         operation.container = container;
+        operation.database = database;
         [operation prepareForRequest];
         ODRequest *request = operation.request;
         expect([request class]).to.beSubclassOf([ODRequest class]);
-        expect(request.action).to.equal(@"hello:world");
+        expect(request.action).to.equal(@"record:fetch");
         expect(request.accessToken).to.equal(container.currentAccessToken);
-        expect(request.payload[@"args"]).to.equal(args);
+        expect(request.payload[@"ids"]).to.equal(@[recordID.canonicalString]);
+        expect(request.payload[@"database_id"]).to.equal(database.databaseID);
+        expect(request.payload).toNot.contain(@"desired_keys");
     });
-
-    it(@"calls lambda with dict args", ^{
-        NSDictionary *args = @{@"name": @"bob"};
-        ODLambdaOperation *operation = [ODLambdaOperation operationWithAction:@"hello:world"
-                                                          dictionaryArguments:args];
+    
+    it(@"multiple record", ^{
+        ODRecordID *recordID1 = [[ODRecordID alloc] initWithRecordType:@"book" name:@"book1"];
+        ODRecordID *recordID2 = [[ODRecordID alloc] initWithRecordType:@"book" name:@"book2"];
+        ODFetchRecordsOperation *operation = [ODFetchRecordsOperation operationWithRecordIDs:@[recordID1, recordID2]];
         operation.container = container;
+        operation.database = database;
         [operation prepareForRequest];
         ODRequest *request = operation.request;
         expect([request class]).to.beSubclassOf([ODRequest class]);
-        expect(request.action).to.equal(@"hello:world");
+        expect(request.action).to.equal(@"record:fetch");
         expect(request.accessToken).to.equal(container.currentAccessToken);
-        expect(request.payload[@"args"]).to.equal(args);
+        expect(request.payload[@"ids"]).to.equal(@[recordID1.canonicalString, recordID2.canonicalString]);
+        expect(request.payload[@"database_id"]).to.equal(database.databaseID);
+        expect(request.payload).toNot.contain(@"desired_keys");
     });
-
+    
     it(@"make request", ^{
-        NSDictionary *args = @{@"name": @"bob"};
-        ODLambdaOperation *operation = [ODLambdaOperation operationWithAction:@"hello:world"
-                                                          dictionaryArguments:args];
-
+        ODRecordID *recordID1 = [[ODRecordID alloc] initWithRecordType:@"book" name:@"book1"];
+        ODRecordID *recordID2 = [[ODRecordID alloc] initWithRecordType:@"book" name:@"book2"];
+        ODFetchRecordsOperation *operation = [ODFetchRecordsOperation operationWithRecordIDs:@[recordID1, recordID2]];
+        
         [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
             return YES;
         } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
             NSDictionary *parameters = @{
                                          @"request_id": @"REQUEST_ID",
-                                         @"result": @{
-                                                 @"message": @"hello bob",
-                                                 }
+                                         @"database_id": database.databaseID,
+                                         @"result": @[
+                                                 @{
+                                                     @"_id": @"book/book1",
+                                                     @"_type": @"record",
+                                                     @"title": @"A tale of two cities",
+                                                     },
+                                                 @{
+                                                     @"_id": @"book/book2",
+                                                     @"_type": @"record",
+                                                     @"title": @"Old man and the sea",
+                                                     }
+                                                 ]
                                          };
             NSData *payload = [NSJSONSerialization dataWithJSONObject:parameters
                                                               options:0
                                                                 error:nil];
-
+            
             return [OHHTTPStubsResponse responseWithData:payload
                                               statusCode:200
                                                  headers:@{}];
         }];
-
+        
         waitUntil(^(DoneCallback done) {
-            operation.lambdaCompletionBlock = ^(NSDictionary *result, NSError *operationError) {
+            operation.fetchRecordsCompletionBlock = ^(NSDictionary *recordsByRecordID, NSError *operationError) {
                 dispatch_async(dispatch_get_main_queue(), ^{
-                    expect([result class]).to.beSubclassOf([NSDictionary class]);
-                    expect(result[@"message"]).to.equal(@"hello bob");
+                    expect([recordsByRecordID class]).to.beSubclassOf([NSDictionary class]);
+                    expect(recordsByRecordID).to.haveCountOf(2);
+                    expect([recordsByRecordID[recordID1] recordID]).to.equal(recordID1);
+                    expect([recordsByRecordID[recordID2] recordID]).to.equal(recordID2);
                     done();
                 });
             };
-
-            [container addOperation:operation];
+            
+            [database executeOperation:operation];
         });
     });
-
+    
     it(@"pass error", ^{
-        NSDictionary *args = @{@"name": @"bob"};
-        ODLambdaOperation *operation = [ODLambdaOperation operationWithAction:@"hello:world"
-                                                             dictionaryArguments:args];
-
+        ODRecordID *recordID1 = [[ODRecordID alloc] initWithRecordType:@"book" name:@"book1"];
+        ODRecordID *recordID2 = [[ODRecordID alloc] initWithRecordType:@"book" name:@"book2"];
+        ODFetchRecordsOperation *operation = [ODFetchRecordsOperation operationWithRecordIDs:@[recordID1, recordID2]];
         [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
             return YES;
         } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
             return [OHHTTPStubsResponse responseWithError:[NSError errorWithDomain:NSURLErrorDomain code:0 userInfo:nil]];
         }];
-
+        
         waitUntil(^(DoneCallback done) {
-            operation.lambdaCompletionBlock = ^(NSDictionary *result, NSError *operationError) {
+            operation.fetchRecordsCompletionBlock = ^(NSDictionary *recordsByRecordID, NSError *operationError) {
                 dispatch_async(dispatch_get_main_queue(), ^{
                     expect(operationError).toNot.beNil();
                     done();
                 });
             };
-
-            [container addOperation:operation];
+            [database executeOperation:operation];
         });
     });
-
+    
+    it(@"per block", ^{
+        ODRecordID *recordID1 = [[ODRecordID alloc] initWithRecordType:@"book" name:@"book1"];
+        ODRecordID *recordID2 = [[ODRecordID alloc] initWithRecordType:@"book" name:@"book2"];
+        ODRecordID *recordID3 = [[ODRecordID alloc] initWithRecordType:@"book" name:@"book3"];
+        ODFetchRecordsOperation *operation = [ODFetchRecordsOperation operationWithRecordIDs:@[recordID1, recordID2]];
+        
+        [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+            return YES;
+        } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
+            NSDictionary *parameters = @{
+                                         @"request_id": @"REQUEST_ID",
+                                         @"database_id": database.databaseID,
+                                         @"result": @[
+                                                 @{
+                                                     @"_id": @"book/book1",
+                                                     @"_type": @"record",
+                                                     @"title": @"A tale of two cities",
+                                                     },
+                                                 @{
+                                                     @"_id": @"book/book2",
+                                                     @"_type": @"error",
+                                                     @"code": @(100),
+                                                     @"message": @"An error.",
+                                                     @"type": @"FetchError",
+                                                     },
+                                                 @{
+                                                     @"_id": @"book/book3",
+                                                     @"_type": @"unknown",
+                                                     },
+                                                 ]
+                                         };
+            NSData *payload = [NSJSONSerialization dataWithJSONObject:parameters
+                                                              options:0
+                                                                error:nil];
+            
+            return [OHHTTPStubsResponse responseWithData:payload
+                                              statusCode:200
+                                                 headers:@{}];
+        }];
+        
+        waitUntil(^(DoneCallback done) {
+            NSMutableArray *remainingRecordIDs = [@[recordID1, recordID2, recordID3] mutableCopy];
+            operation.perRecordCompletionBlock = ^(ODRecord *record, ODRecordID *recordID, NSError *error) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if ([recordID isEqual:recordID1]) {
+                        expect([record class]).to.beSubclassOf([ODRecord class]);
+                        expect(record.recordID).to.equal(recordID1);
+                    } else if ([recordID isEqual:recordID2]) {
+                        expect([error class]).to.beSubclassOf([NSError class]);
+                        expect([error ODErrorType]).to.equal(@"FetchError");
+                    } else if ([recordID isEqual:recordID3]) {
+                        expect([error class]).to.beSubclassOf([NSError class]);
+                    }
+                    [remainingRecordIDs removeObject:recordID];
+                });
+            };
+            
+            operation.fetchRecordsCompletionBlock = ^(NSDictionary *recordsByRecordID, NSError *operationError) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    expect(remainingRecordIDs).to.haveCountOf(0);
+                    done();
+                });
+            };
+            
+            
+            [database executeOperation:operation];
+        });
+    });
+    
     afterEach(^{
         [OHHTTPStubs removeAllStubs];
     });
