9fd1ea23d188b5190fd9840cb6cce5cc411be9ee
Ben Lei <benlei@oursky.com>
2016-02-29 12:50:41 +0800

    Enable fixing clang format in `clang-check.sh`

--- clang-check.sh
+++ clang-check.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-find ./Pod ./Example/Tests -name "*.[hm]" -exec clang-format -style=file -output-replacements-xml "{}" \; | grep "<replacement " >/dev/null
-if [ $? -ne 1 ]
-then 
-    echo "Commit did not match clang-format"
-    exit 1;
+if [ "$1" == "fix" ]
+then
+    echo "Fixing Clang Format..."
+    find ./Pod ./Example/Tests -name "*.[hm]" -exec clang-format -i -style=file "{}" \;
+else
+    echo "Checking Clang Format..."
+    find ./Pod ./Example/Tests -name "*.[hm]" -exec clang-format -style=file -output-replacements-xml "{}" \; | grep "<replacement " >/dev/null
+    if [ $? -ne 1 ]
+    then
+        echo "Commit did not match clang-format"
+        exit 1;
+    fi
 fi
