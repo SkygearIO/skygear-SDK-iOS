a2f92e3199f6bf353e80cdde76ffc0784f563c19
Ben Lei <benlei@oursky.com>
2017-01-05 16:43:04 +0800

    Add device registration to example project

--- Example/Swift Example/AppDelegate.swift
+++ Example/Swift Example/AppDelegate.swift
@@ -36,28 +36,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if let apiKeyValue = apiKey {
             SKYContainer.defaultContainer().configureWithAPIKey(apiKeyValue)
         }
+
+        application.registerUserNotificationSettings(UIUserNotificationSettings(
+            forTypes: [.Alert, .Badge, .Sound],
+            categories: nil
+        ))
+
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+        guard !notificationSettings.types.isEmpty else {
+            print("User does not allow notification")
+            return
+        }
+
+        application.registerForRemoteNotifications()
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        print("Successfully registered remote notification")
+
+        let skygear = SKYContainer.defaultContainer()
+        guard skygear.currentUser != nil else {
+            print("User not yet login, abort registering device")
+            return
+        }
+
+        skygear.registerRemoteNotificationDeviceToken(deviceToken) { (deviceID, error) in
+            guard error == nil else {
+                print("Failed to register device: \(error.localizedDescription)")
+                return
+            }
+
+            if let id = deviceID {
+                print("Successfully registered device with ID: \(id)")
+            } else {
+                print("Warning: Got nil device ID")
+            }
+        }
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        print("Failed to register remote notification: \(error.localizedDescription)")
     }
 }
