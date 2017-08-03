5318a6eb26cba75d19ca14d2a4a854838fb7d1bd
Kwok-kuen Cheung <cheungpat@oursky.com>
2017-08-03 16:21:25 +0800

    Upload doc prefixed with version to s3 bucket for CI
    Examples:
    
    * https://docs.skygear.io/ios/reference/v1.1.0/
    * https://docs.skygear.io/ios/reference/latest/
    * https://docs.skygear.io/ios/reference/canary/

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+# Update docs.skygear.io
+
+if [ -n "$TRAVIS_TAG" ]; then
+    if [ "$TRAVIS_TAG" -eq "latest" ]; then
+        make doc-deploy VERSION=latest GIT_REF_NAME=latest
+    else
+        make doc-deploy VERSION="v$TRAVIS_TAG" GIT_REF_NAME="$TRAVIS_TAG"
+    fi
+else
+    make doc-deploy VERSION="${TRAVIS_BRANCH/master/canary}" GIT_REF_NAME="$TRAVIS_BRANCH"
+fi
