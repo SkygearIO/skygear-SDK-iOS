415a4521cd6e695a1b352aef300207433c66594a
stevenchan <stevenchan@oursky.com>
2017-08-04 18:04:27 +0800

    Add Undocumented annotation to containers interface

--- Pod/Classes/SKYPublicDatabase.h
+++ Pod/Classes/SKYPublicDatabase.h
@@ -21,6 +21,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// Undocumented
 @interface SKYPublicDatabase : SKYDatabase
 
 /**
--- Pod/Classes/SKYPubsubContainer.h
+++ Pod/Classes/SKYPubsubContainer.h
@@ -21,6 +21,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// Undocumented
 @interface SKYPubsubContainer : NSObject
 
 @property (nonatomic, readonly) NSURL *_Nullable endPointAddress;
