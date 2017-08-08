c8002368e73a0070a869317186e33c197875e7c7
Kwok-kuen Cheung <cheungpat@oursky.com>
2017-08-08 13:26:26 +0800

    Fix doc site deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 # Update docs.skygear.io
 
 if [ -n "$TRAVIS_TAG" ]; then
-    if [ "$TRAVIS_TAG" -eq "latest" ]; then
+    if [ "$TRAVIS_TAG" == "latest" ]; then
         make doc-deploy VERSION=latest GIT_REF_NAME=latest
     else
         make doc-deploy VERSION="v$TRAVIS_TAG" GIT_REF_NAME="$TRAVIS_TAG"
